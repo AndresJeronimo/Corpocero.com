f8d633603bd8f8b263753f10f9f9e56133c0dee4
AndresJeronimo <arico8199@gmail.com>
2025-07-09 14:29:22 -0500

    Update buscador.js

--- JS/buscador.js
+++ JS/buscador.js
@@ -3,16 +3,16 @@ function buscar() {
 
       // Redireccionar según la palabra clave ingresada
     if (palabra === "publicidad") {
-        window.location.href = "/publicidad.html";
+        window.location.href = "publicidad.html";
 
     } else if (palabra === "prestaciones" || palabra === "servicios") {
         window.location.href = "prestacionesServicios.html";
 
     } else if (palabra === "otros servicios") {
-        window.location.href = "/otrosServicios.html";
+        window.location.href = "otrosServicios.html";
 
     } else if (palabra === "obras") {
-        window.location.href = "/nuestrasObras.html";
+        window.location.href = "nuestrasObras.html";
 
     } else {
         // Página por defecto o de "no encontrado"
